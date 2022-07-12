427a1fc519931efe97843073ab821b7535d09466
minhtuan <tuannm412@gmail.com>
2022-07-12 17:25:21 +0700

    create login and post view

---
+++ project/components/post/PostPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="post">
+    <div class="page-name">
+      投稿
+    </div>
+    <div class="md-layout post-layout">
+      <div class="md-layout-item">
+        <div class="post-box">
+            <span class="post-text">どんな出来ばえ？</span>
+        </div>
+        <div class="info">
+            <img :src="POST" />
+            <span class="post-text">写真を選択</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import POST from '@/assets/icons/post.png'
+export default {
+  name: 'PostPage',
+  components: {
+  },
+  mixins: [],
+  props: {},
+  data() {
+    return {
+        POST
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/post";
+</style>
---
+++ project/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :key="keyRender" class="body-wrapper">
+      <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault',
+  components: {},
+  mixins: [],
+  middleware: [],
+  data() {
+    return {
+      keyRender: new Date().getTime(),
+    }
+  },
+  computed: {},
+}
+</script>
+<style lang="scss">
+.body-wrapper {
+  overflow: hidden;
+}
+
+body {
+  width: 375px !important;
+  margin: 0 auto;
+  padding-left: 5px;
+  padding-right: 5px;
+  background-color: #fff;
+}
+
+.wrapper {
+  height: calc(100vh - 12px - 64px);
+  overflow-y: scroll;
+  overflow-x: none;
+}
+</style>
