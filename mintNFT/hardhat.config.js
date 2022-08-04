032879d15029c0a5eba7f0274c28caf5a25c1240
minhtuan <tuannm412@gmail.com>
2022-08-04 10:04:12 +0700

    update mintNFT

--- mintNFT/hardhat.config.js
+++ mintNFT/hardhat.config.js
@@ -6,9 +6,9 @@ require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
    solidity: "0.8.9",
-   defaultNetwork: "ganache",
+   defaultNetwork: "localhost",
    networks: {
-      ganache: {
+      localhost: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
